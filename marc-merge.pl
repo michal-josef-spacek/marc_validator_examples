aeb9b57e7ddfd6185d9b2b68a902b16211f847d3
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2026-02-24 19:18:31 +0100

    Prepare for 0.05 version

--- marc-merge.pl
+++ marc-merge.pl
@@ -8,7 +8,7 @@ use Error::Pure qw(err);
 use File::Find::Rule;
 use MARC::File::XML (BinaryEncoding => 'utf8', RecordFormat => 'MARC21');
 
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 my @xml_files = File::Find::Rule->file->in('examples');
 
